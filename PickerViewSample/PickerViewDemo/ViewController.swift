3299d96a2f12bfaf58db0789828ead4fabd1b085
Rasheda Babatunde <RJ@Rashedas-MacBook-Pro.local>
2016-09-19 14:22:37 -0500

    updated to swift 3

--- PickerViewSample/PickerViewDemo/ViewController.swift
+++ PickerViewSample/PickerViewDemo/ViewController.swift
@@ -22,32 +22,32 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
 
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
         
         return 1
         
     }
     
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         
         return pickerColors.count
         
     }
     
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
         return pickerColors[row]
         
     }
     
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
         switch row {
-        case 0: self.view.backgroundColor = UIColor.whiteColor()
-        case 1: self.view.backgroundColor = UIColor.redColor()
-        case 2: self.view.backgroundColor = UIColor.greenColor()
-        case 3: self.view.backgroundColor = UIColor.purpleColor()
-        default: self.view.backgroundColor = UIColor.lightGrayColor()
+        case 0: self.view.backgroundColor = UIColor.white
+        case 1: self.view.backgroundColor = UIColor.red
+        case 2: self.view.backgroundColor = UIColor.green
+        case 3: self.view.backgroundColor = UIColor.purple
+        default: self.view.backgroundColor = UIColor.lightGray
         }
         
     }
